ad814279e6327aea77a0ec1fe898b074eeded692
Rafael Arantes <rafael-fullstack@outlook.com>
2023-02-14 15:49:57 -0300

    Melhorias no código

---
+++ Repositorios/CriarPedidoViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TesteWebApi.Repositorios
+{
+    public class CriarPedidoViewModel
+    {
+        public int UsuarioId { get; set; }
+        public List<CriarPedidoItemViewModel> Produtos { get; set; }
+    }
+
+    public class CriarPedidoItemViewModel
+    {
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
